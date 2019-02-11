1bc01747a4de3f891cde216fbedc1658840b2d58
Abhisek-Lenka <47021991+Abhisek-Lenka@users.noreply.github.com>
2019-02-11 16:43:17 +0530

    Delete losangeleschckelement.js

--- Test Cases/losangeleschckelement.js
+++
@@ -1,3 +0,0 @@
-// some filters are not available for every locations
-//i.e if we search for Los angeles, then the free delivery and coupons filter are not available
-//write a test case if the elements are visible then click them.
